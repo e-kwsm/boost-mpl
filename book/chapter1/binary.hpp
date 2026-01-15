3a5a49cd4425cd00c94290159bf2e0e128093af4
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-01-15 19:06:46 +0900

    book/chapter1/binary.hpp

--- book/chapter1/binary.hpp
+++ book/chapter1/binary.hpp
@@ -7,12 +7,11 @@
 #include "boost/mpl/long.hpp"
 #include "boost/mpl/alias.hpp"
 
-template< long n > struct binary
-    : mpl::long_< ( binary< n / 10 >::value << 1 ) + n % 10 >
+template <long n>
+struct binary : mpl::long_<(binary<n / 10>::value << 1) + n % 10>
 {
 };
 
-template<> struct binary<0>
-    : mpl::long_<0>
+template <> struct binary<0> : mpl::long_<0>
 {
 };
